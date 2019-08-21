6b69480ed4ac0e14599e95b9583c9a039723f8aa
github username <github email address>
2019-08-20 21:53:20 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,56 @@
-
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, "whatever"
+    end
 
-  get '/' do
-  end
+    # Index
+    get '/articles' do
+        @articles = Article.all
+        erb :index
+    end
+
+    # New
+    get '/articles/new' do
+        erb :new
+    end
+
+    # Create
+    post '/articles' do
+        @article = Article.create(params)    
+        redirect "/articles/#{@article.id}"
+    end
+
+    # Show
+    get '/articles/:id' do
+        @article = Article.find(params[:id])
+        erb :show
+    end
+
+    # Edit - pulls up the edit form.
+    get '/articles/:id/edit' do
+        @article = Article.find(params[:id])
+        erb :edit
+    end
+
+    # Update - updates the data upon submit.
+    # * Note: update now takes in the specific params of [:article]
+    patch '/articles/:id' do
+        @article = Article.find(params[:id])
+        @article.update(params[:article])
+        redirect "/articles/#{@article.id}"
+    end
+
+    # Destroy
+    delete '/articles/:id/delete' do
+       @article = Article.find(params[:id])
+       @article.destroy
+       redirect "/articles"        
+    end
+
 end
