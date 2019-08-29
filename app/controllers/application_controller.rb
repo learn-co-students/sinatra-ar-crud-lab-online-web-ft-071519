414c02836bdbbd0b4e2105e32009a37c03233e5b
Emily Lillie <emilylillie3@gmail.com>
2019-08-29 19:31:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,49 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+  
+ get "/articles" do
+    @articles = Article.all
+    erb :index
+  end
+
+   # new
+  get "/articles/new" do
+    @article = Article.new
+    erb :new
+  end
+
+   # create
+  post "/articles" do
+    @article = Article.create(params)
+    redirect to "/articles/#{ @article.id }"
+  end
+
+ # read
+  # show
+  get "/articles/:id" do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+   # update
+  patch "/articles/:id" do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{ @article.id }"
+  end
+
+   # edit
+  get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+   #destroy
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
+  end	  
+  
+  
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,12 @@
 #Placeholder for a model
 
+# class CreateArticles < ActiveRecord::Migration[5.2]
+#   def change 
+#     create_table articles do |t|
+#       t.string = :title 
+#       t.string = :content 
+#     end 
+#   end 
+
 class Article < ActiveRecord::Base
 end
